1a71042520c9f2835f95ea31811725eab72d17b8
Akshay4955 <akshayshedge11@gmail.com>
2023-05-21 14:52:17 +0530

    Added: Code to check employee attendance

--- EmployeeWageCalculator/Program.cs
+++ EmployeeWageCalculator/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace EmployeeWageCalculator
+﻿using System.Security.Cryptography;
+
+namespace EmployeeWageCalculator
 {
     internal class Program
     {
+        const int IS_PRESENT = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage calculator...!!!");
+            getAttendance();
+        }
+        static void getAttendance()
+        {
+            Random random = new Random();
+            if (random.Next(2) == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
